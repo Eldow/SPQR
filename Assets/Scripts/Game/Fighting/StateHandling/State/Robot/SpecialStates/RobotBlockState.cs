a91a593eefdec37e2b5aed8dd580584808f08316
m4jr0 <lowgravityisfun@gmail.com>
2017-02-16 10:47:51 -0500

    You can hold the Block State now.

--- Assets/Scripts/Game/Fighting/StateHandling/State/Robot/SpecialStates/RobotBlockState.cs
+++ Assets/Scripts/Game/Fighting/StateHandling/State/Robot/SpecialStates/RobotBlockState.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class RobotBlockState : RobotState {
     public override RobotState HandleInput(RobotStateMachine stateMachine, 
@@ -6,6 +7,17 @@ public class RobotBlockState : RobotState {
         if (!this.IsAnimationPlaying(stateMachine, "RobotBlock")) {
             return null;
         }
+
+        if (this.CheckIfBlockHolding(xboxInput)) {
+            if (this.IsCurrentAnimationPlayedPast(stateMachine, .5f) && 
+                Math.Abs(stateMachine.Animator.speed) > .01f) {
+                this.FreezeAnimation(stateMachine);
+            }
+
+            return null;
+        }
+
+        this.ResumeAnimation(stateMachine);
 
         if (this.IsCurrentAnimationFinished(stateMachine)) {
             return new RobotIdleState();
@@ -26,5 +38,9 @@ public class RobotBlockState : RobotState {
     public override void Exit(RobotStateMachine stateMachine) {
         Debug.Log("BLOCK EXIT!");
         stateMachine.Animator.SetBool("IsBlock", false);
+    }
+
+    protected virtual bool CheckIfBlockHolding(XboxInput xboxInput) {
+        return Input.GetKey(xboxInput.B);
     }
 }
