b41b225a471670cb625ac023a85d7b535fadf590
m4jr0 <lowgravityisfun@gmail.com>
2017-02-17 17:44:34 -0500

    Changed smoothing factor.

--- Assets/Scripts/Scenes/MenuHandling/MainMenuManager.cs
+++ Assets/Scripts/Scenes/MenuHandling/MainMenuManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour {
     private SceneStateMachine _sceneStateMachine = null;
