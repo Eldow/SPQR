209c8782d0fd72ae10aa6b9a77691d2fd6a602cc
m4jr0 <lowgravityisfun@gmail.com>
2017-02-17 12:58:03 -0500

    Added SandboxSceneState.

---
+++ Assets/Scripts/Game/Fighting/StateHandling/State/RootScene/SceneState.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class SceneState : State {
+    public string SceneName { get; protected set; }
+
+    // to be changed in a child class, if necessary
+    public virtual string DefaultSceneName {
+        get {
+            return "";
+        }
+    }
+
+    public SceneState(string sceneName = null) {
+        if (String.IsNullOrEmpty(sceneName)) {
+            sceneName = this.DefaultSceneName;
+        }
+
+        this.SceneName = sceneName;
+    }
+
+    public virtual void Update(SceneStateMachine stateMachine) {
+
+    }
+
+    protected virtual void 
+        Enter(SceneStateMachine stateMachine) {
+        if (!stateMachine.LoadScene(this.SceneName)) {
+            Debug.LogError(this.SceneName + ": unknown scene to load!");
+        }
+    }
+
+    public virtual void Exit(SceneStateMachine stateMachine) {
+        if (!stateMachine.UnloadScene(this.SceneName)) {
+            Debug.LogError(this.SceneName + ": error while unloading!");
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Fighting/StateHandling/StateMachine/RootScene/SceneStateMachine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneStateMachine : StateMachine {
+    public string StateToLoadFirst = "";
+    protected SceneState SceneState = null;
+
+    // to be changed in a child class, if necessary
+    public virtual string DefaultState {
+        get {
+            return "State";
+        }
+    }
+
+    void Start() {
+        this.Initialize(this.StateToLoadFirst);
+    }
+
+    void FixedUpdate() {
+        this.SceneState.Update(this);
+    }
+
+    protected override void Initialize(string startingState = null) {
+        Type stateType = this.CheckStartingState(startingState);
+
+        if (stateType == null) return;
+
+        this.SceneState = 
+            (SceneState) Activator.CreateInstance(stateType);
+        this.SceneState.Enter(this);
+    }
+
+    public virtual bool LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+
+        return SceneManager.GetActiveScene().name == sceneName;
+    }
+
+    public virtual bool UnloadScene(string sceneName) {
+        AsyncOperation operation = SceneManager.UnloadSceneAsync(sceneName);
+
+        return operation.isDone; // to change
+    }
+
+    protected virtual void SwitchState(SceneState sceneState) {
+        if (sceneState == null) return;
+
+        this.SceneState.Exit(this);
+        this.SceneState = sceneState;
+        this.SceneState.Enter(this);
+    }
+
+    public virtual void SetState(SceneState sceneState) {
+        this.SwitchState(sceneState);
+    }
+}
