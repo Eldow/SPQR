52c65fe85623dd1ead3eeacf4251afbbd7d86bc6
m4jr0 <lowgravityisfun@gmail.com>
2017-03-01 18:25:40 -0500

    Hitstun animation is now framed.

--- Assets/Scripts/Game/Fighting/StateHandling/State/Robot/SpecialStates/RobotHitstunState.cs
+++ Assets/Scripts/Game/Fighting/StateHandling/State/Robot/SpecialStates/RobotHitstunState.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
+
 public class RobotHitstunState : RobotFramedState {
+    protected float InitialSpeed = 0;
+    protected bool IsSpeedSet = false;
+
     protected override void Initialize() {
         this.IASA = this.MaxFrame;
         this.MinActiveState = 0;
@@ -22,6 +26,8 @@ public class RobotHitstunState : RobotFramedState {
             return null;
         }
 
+        if (!this.IsSpeedSet) this.SetSpeed(robotStateMachine);
+
         if (!this.IsStateFinished()) return null;
 
         if (this.IsLastState(robotStateMachine, "RobotWalkState")) {
@@ -36,8 +42,28 @@ public class RobotHitstunState : RobotFramedState {
     }
 
     public override void Enter(StateMachine stateMachine) {
+        if (!(stateMachine is RobotStateMachine)) return;
+
+        RobotStateMachine robotStateMachine = (RobotStateMachine) stateMachine;
+
+        this.InitialSpeed = robotStateMachine.Animator.speed;
     }
 
     public override void Exit(StateMachine stateMachine) {
+        if (!(stateMachine is RobotStateMachine)) return;
+
+        RobotStateMachine robotStateMachine = (RobotStateMachine)stateMachine;
+
+        robotStateMachine.Animator.speed = this.InitialSpeed;
+    }
+
+    protected virtual void SetSpeed(RobotStateMachine robotStateMachine) {
+        float desiredAnimationTime = this.MaxFrame / (1 / Time.fixedDeltaTime);
+
+        robotStateMachine.Animator.speed = 
+            (robotStateMachine.Animator.GetCurrentAnimatorStateInfo(0).length *
+            robotStateMachine.Animator.speed) / desiredAnimationTime;
+
+        this.IsSpeedSet = true;
     }
 }
