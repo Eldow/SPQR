948e72fce92bea1ed639f9811e27de35a010cbb0
Eldow <francois.decan@free.fr>
2017-04-11 23:04:41 -0400

    Master client can now add bot to his room

--- Assets/Scripts/UI/Lobby/MatchmakingPanelBehaviour.cs
+++ Assets/Scripts/UI/Lobby/MatchmakingPanelBehaviour.cs
@@ -6,6 +6,7 @@ public class MatchmakingPanelBehaviour : MonoBehaviour {
 
     public GameObject PlayButton;
     public GameObject LeaveButton;
+    public GameObject AddBotButton;
     
     void Start()
     {
@@ -16,11 +17,13 @@ public class MatchmakingPanelBehaviour : MonoBehaviour {
     {
         PlayButton.SetActive(true);
         LeaveButton.SetActive(false);
+        AddBotButton.SetActive(true);
     }
 
     public void InFriendRoom()
     {
         PlayButton.SetActive(false);
         LeaveButton.SetActive(true);
+        AddBotButton.SetActive(false);
     }
 }
