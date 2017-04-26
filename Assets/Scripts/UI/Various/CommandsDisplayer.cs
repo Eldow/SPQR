3997019510ad6fcf4f4546827cbcd7b532e06274
m4jr0 <lowgravityisfun@gmail.com>
2017-04-26 14:17:29 -0400

    Added commands panel.

---
+++ Assets/Scripts/UI/Various/CommandsDisplayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandsDisplayer : MonoBehaviour {
+    public GameObject KeyBinds = null;
+
+    public void Display() {
+        if (this.KeyBinds == null) return;
+
+        this.KeyBinds.SetActive(true);
+    }
+
+    public void Hide() {
+        if (this.KeyBinds == null) return;
+
+        this.KeyBinds.SetActive(false);
+    }
+}
